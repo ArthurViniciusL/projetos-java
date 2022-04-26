858b08c0920c7414ebcdfb5bea02e8da6c4efb81
ArthurViniciusSL <76112437+ArthurViniciusL@users.noreply.github.com>
2022-04-25 21:04:56 -0300

    25/04/22

---
+++ gerador-de-senhas/src/senhas/Main.java
@@ -0,0 +1,16 @@
+package senhas;
+
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String[] args) {
+
+		ArrayList<Character> lista = new ArrayList<Character>();
+
+		for (char simb = '!'; simb <= '~'; simb++) {
+			lista.add(simb);
+		}
+		
+		
+	}
+}
