c7cd29db08f5e2a8551b78bab1354958dceec2bf
ArthurViniciusSL <76112437+ArthurViniciusL@users.noreply.github.com>
2022-04-28 22:54:31 -0300

    Array simples

---
+++ estruturas_de_dados/src/arrays/MeuArraySimples.java
@@ -0,0 +1,12 @@
+package arrays;
+
+//import lombok.Data;
+//@Data
+public class MeuArraySimples {
+
+    private String[] elementosVetor;
+
+    public MeuArraySimples(int tamanho) {
+    	this.elementosVetor = new String[tamanho];
+    }
+}
---
+++ estruturas_de_dados/src/testes/Aula002.java
@@ -0,0 +1,13 @@
+package testes;
+
+import arrays.MeuArraySimples;
+
+public class Aula002 {
+
+	public static void main(String[] args) {
+	
+		MeuArraySimples arraySimples = new MeuArraySimples(5);
+		int i = 0;
+		i++;
+	}
+}
